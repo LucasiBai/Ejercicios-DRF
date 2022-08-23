24eb50ec6bb3d956f0df556158151d982c07ecf4
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-08-23 15:31:04 -0300

    put permitido solo al creador

--- Ej4/data/api/views/hero_viewsets.py
+++ Ej4/data/api/views/hero_viewsets.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from data.api.serializers import HeroSerializer
 from data.models import Hero
@@ -7,3 +8,25 @@ from data.models import Hero
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by("name")
     serializer_class = HeroSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def update(self, request, pk, *args, **kwargs):
+        # User filter
+        user = request.user
+        hero = Hero.objects.filter(id=pk).first()
+        if user.id == hero.owner.id:
+            partial = kwargs.pop("partial", False)
+            instance = self.get_object()
+            serializer = self.get_serializer(
+                instance, data=request.data, partial=partial
+            )
+            if serializer.is_valid():
+                self.perform_update(serializer)
+                return Response(serializer.data)
+        return Response(
+            "Debe ser el usuario creador para modificarlo",
+            status=status.HTTP_400_BAD_REQUEST,
+        )
--- Ej4/data/models.py
+++ Ej4/data/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     alias = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        "auth.User", related_name="heroes", on_delete=models.CASCADE
+    )
 
     def __str__(self) -> str:
         return self.name
